375d3150f558e7950ec2a3f130baf4aa0b318821
karshann <karshan951@gmail.com>
2020-03-26 12:32:50 +0100

    creation des class test et fin des class vendeur , manager

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class Manager {
+public class Manager extends Employe {
+    int ordre;
+    public Manager(int anciennete, int ordre) {
+        super(anciennete);
+        this.ordre=ordre;
+    }
+
+    @Override
+    public int calculsalaire() {
+        return super.calculsalaire()+100*ordre;
+    }
 }
--- src/main/java/org/example/Vendeur.java
+++ src/main/java/org/example/Vendeur.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class Vendeur {
+public class Vendeur extends Employe {
+    private int commission;
+
+    public Vendeur(int commission,int anciennete) {
+        super(anciennete);
+        this.commission = commission;
+    }
+
+    @Override
+    public int calculsalaire() {
+        return super.calculsalaire()+commission;
+    }
 }
