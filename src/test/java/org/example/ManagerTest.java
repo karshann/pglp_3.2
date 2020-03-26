c292d6eac94a9c9e91ba812a88237904c7817d05
karshann <karshan951@gmail.com>
2020-03-26 12:37:58 +0100

    test manager

--- src/test/java/org/example/ManagerTest.java
+++ src/test/java/org/example/ManagerTest.java
@@ -6,4 +6,10 @@ import static org.junit.Assert.*;
 
 public class ManagerTest {
 
+    @Test
+    public void calculsalaire() {
+
+        Manager m=new Manager(0,5);
+        assertEquals(2000,m.calculsalaire());
+    }
 }
